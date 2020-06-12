6a9e3602f73d4f973621de60756727ad792acee2
samzadel <samzadel@hotmail.fr>
2020-06-12 18:43:15 +0300

    Remove suggestions when click outside

--- components/Search2/search.js
+++ components/Search2/search.js
@@ -1,7 +1,10 @@
 import styles from './search.module.css'
-import React, { useState } from 'react';
-import _, { debounce } from 'lodash';
-
+import React, { useState, useRef, useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { config } from '@fortawesome/fontawesome-svg-core'
+import { dom } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false
 
 export default function Search() {
 
@@ -9,12 +12,26 @@ export default function Search() {
 
   const [valueClick, setValueClick] = useState('')
 
-  const sendRequest = debounce(async (value) => {
-    const url = `http://localhost:3100/`
-    if (value === '') {
-      setState([])
-      return
+  const [isVisible, setVisible] = useState(true)
+
+  const suggestions = useRef(null)
+
+  useEffect(() => {
+    document.addEventListener("click", catchClick)
+    return () => {
+      document.removeEventListener("click", catchClick)
     }
+  }, [])
+
+  const catchClick = (event) => {
+    if (suggestions.current && !suggestions.current.contains(event.target)) {
+      setVisible(false)
+    }
+
+  }
+
+  const sendRequest = async (value) => {
+    const url = `http://localhost:3100/`
     let response = await fetch(url, {
       headers: {
         'Content-Type': 'application/json'
@@ -24,29 +41,39 @@ export default function Search() {
     })
     let test = await response.json()
     setState(test)
-  },500)
+  }
 
-  const handleChange = (content)=>{
+  const handleChange = (content) => {
+    setVisible(true)
     setValueClick(content)
     sendRequest(content)
+    if (content === '') {
+      setState([])
+    }
   }
 
 
   const getSuggest = (value) => {
     setValueClick(value)
+    setState([])
   }
 
   return (
     <>
+      <h2 className={styles.title}>Search Places</h2>
       <div className={styles.suggestion_input}>
-        <h2>Search Places</h2>
         <input placeholder='חפשו איזור, שכונה או בי״ס' className={styles.input_suggests} onChange={e => handleChange(e.target.value)} value={valueClick} />
-        {
-          state.map((item, index) => {
-            return <div className={styles.suggests} onClick={e => getSuggest(item.description)} key={index}>{item.description} <span className={styles.suggests_type}>{item.type}</span>
+        <button className={styles.iconSearch}><FontAwesomeIcon icon={faSearch} width={40} /></button>
+          {(state.length > 0 && isVisible) &&
+            <div ref={suggestions}>
+              {
+                state.map((item, index) => {
+                  return <div className={styles.suggests} onClick={e => getSuggest(item.description)} key={index}>{item.description} <span className={styles.suggests_type}>{item.type}</span>
+                  </div>
+                })
+              }
             </div>
-          })
-        }
+          } 
       </div>
     </>
   )
